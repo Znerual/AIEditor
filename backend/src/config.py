9f4223dbd7c036e999739aad8c04256f4e0a1aa7
Laurenz Ruzicka <laurenz_ruzicka@gmx.at>
2024-12-07 17:02:33 +0100

    added more options for localhost

--- backend/src/config.py
+++ backend/src/config.py
@@ -2,4 +2,4 @@ class Config:
     SECRET_KEY = 'secret!'
     DEBUG = True
     # Allow connections from React development server
-    CORS_ORIGINS = ["http://localhost:3000", "http://127.0.0.1:3000", "*"] # http://localhost:3000
+    CORS_ORIGINS = ["http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:3000/", "*"] # http://localhost:3000
