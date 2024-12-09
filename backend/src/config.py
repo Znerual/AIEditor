d26f3d86e1c4a43e72c6c900414eeee94572921a
Laurenz Ruzicka <laurenz_ruzicka@gmx.at>
2024-12-09 08:33:16 +0100

    changed password and username for postgres database

--- backend/src/config.py
+++ backend/src/config.py
@@ -3,5 +3,6 @@ class Config:
     DEBUG = True
     # Allow connections from React development server
     CORS_ORIGINS = ["http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:3000/", "*"] # http://localhost:3000
-    SQLALCHEMY_DATABASE_URI = 'postgresql://username:password@localhost:5432/your_database'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1234@localhost:5432/eddy_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
