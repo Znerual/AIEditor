039bbca0305b584ddc82ed1e3e261ef487996223
Laurenz Ruzicka <laurenz_ruzicka@gmx.at>
2024-12-09 14:25:35 +0100

    naviate

--- frontend/src/hooks/useWebSocket.js
+++ frontend/src/hooks/useWebSocket.js
@@ -2,12 +2,14 @@
 import { useEffect, useCallback, useState, useRef } from 'react';
 import { socketService } from '../services/socket.service';
 import { useAuth } from '../contexts/AuthContext';
+import { useNavigate } from 'react-router-dom';
 
 export const useWebSocket = (events) => {
     const { token } = useAuth(); // Get token from auth context
     const [status, setStatus] = useState(socketService.getStatus());
     const [debugEvents, setDebugEvents] = useState([]);
     const eventsRef = useRef(events);
+    const navigate = useNavigate();
 
     // Update events ref when events change
     useEffect(() => {
@@ -19,15 +21,16 @@ export const useWebSocket = (events) => {
             socketService.disconnect();
             setStatus('unauthorized');
             console.log('Disconnect because there is no access token, login first')
+            navigate('/'); // send user to login form
             return;
         }
 
         const socket = socketService.connect();
         if (!socket) return;
 
-        socket.on('connect', () => {
-            
-            socket.emit('authentication', { token }); // Emit authentication event with token
+        // Start the authentication handshake with the jwt
+        socket.on('server_connects', () => {
+            socket.emit('client_authenticates', { token }); // Emit authentication event with token
         });
 
         // Setup status listener
@@ -36,31 +39,31 @@ export const useWebSocket = (events) => {
         };
         socketService.addStatusListener(handleStatus);
 
-        // // Wrap each event handler to track debug events
-        // const wrappedHandlers = Object.entries(events).reduce((acc, [event, handler]) => {
-        //     acc[event] = (...args) => {
-        //         // Add to debug events
-        //         setDebugEvents(prev => [...prev, {
-        //             event,
-        //             data: args,
-        //             type: 'received',
-        //             timestamp: new Date().toISOString()
-        //         }]);
+        // Wrap each event handler to track debug events
+        const wrappedHandlers = Object.entries(events).reduce((acc, [event, handler]) => {
+            acc[event] = (...args) => {
+                // Add to debug events
+                setDebugEvents(prev => [...prev, {
+                    event,
+                    data: args,
+                    type: 'received',
+                    timestamp: new Date().toISOString()
+                }]);
                 
-        //         // Call original handler
-        //         handler(...args);
+                // Call original handler
+                handler(...args);
 
-        //         if (typeof handler === 'function') {  // Check if handler is a function
-        //             console.log(`Attached listener for event: ${event}`);
-        //         } else {
-        //             console.error(`Handler for event '${event}' is not a function:`, handler);
-        //         }
-        //     };
-        //     return acc;
-        // }, {});
+                if (typeof handler === 'function') {  // Check if handler is a function
+                    console.log(`Attached listener for event: ${event}`);
+                } else {
+                    console.error(`Handler for event '${event}' is not a function:`, handler);
+                }
+            };
+            return acc;
+        }, {});
 
         // Setup event listeners
-        Object.entries(events).forEach(([event, handler]) => {
+        Object.entries(wrappedHandlers).forEach(([event, handler]) => {
             socket.on(event, handler);
             console.log(`[WebSocket] Registered listener for: ${event}`);
         });
