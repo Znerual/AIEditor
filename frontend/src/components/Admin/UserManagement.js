0945f98694b973eba50f96e0c8933b2912f2b5b4
Laurenz Ruzicka <Laurenz.Ruzicka@ait.ac.at>
2024-12-12 15:33:26 +0100

    Fix missing useAuth imoprt

--- frontend/src/components/Admin/UserManagement.js
+++ frontend/src/components/Admin/UserManagement.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useAuth } from '../../contexts/AuthContext';
 
 export const UserManagement = () => {
     const [users, setUsers] = useState([]);
