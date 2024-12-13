a7792783bd21e06ad1f4398003399ad3fb30c45f
Laurenz Ruzicka <Laurenz.Ruzicka@ait.ac.at>
2024-12-13 14:15:22 +0100

    Improve

--- backend/src/embedding_manager.py
+++ backend/src/embedding_manager.py
@@ -1,17 +1,11 @@
 # src/embedding_manager.py
 import hashlib
 import google.generativeai as genai
-from models import db, FileEmbedding, SequenceEmbedding
+from models import Document, db, FileEmbedding, SequenceEmbedding, FileContent
 from concurrent.futures import ThreadPoolExecutor
-from typing import List, Optional, Tuple
+from typing import List, Optional, Tuple, Union
 
 class EmbeddingManager:       
-    @staticmethod
-    def _extract_text_from_file(filepath: str) -> str:
-        """Extract text from a file."""
-        # Placeholder for actual implementation
-        return ""
-
     @staticmethod
     def _split_text(text: str) -> List[str]:
         """Split text into smaller segments suitable for embedding."""
@@ -30,68 +24,83 @@ class EmbeddingManager:
         return embedding["embedding"], hash
     
     @staticmethod
-    def _calculate_hash(content: str) -> str:
-        """Calculate the SHA-256 hash of the given content."""
-        return hashlib.sha256(content.encode()).hexdigest()
+    def _calculate_hash(text_content: str) -> str:
+        """Generate sha-256 hash"""
+        return hashlib.sha256(text_content).hexdigest()
+    
 
     @staticmethod
-    def get_embeddings(filepath: str, document_id: Optional[int] = None, non_text_content: Optional[bytes] = None) -> Tuple[List[List[float]], List[str]]:
+    def _get_file_content_embeddings(file_content : FileContent) > Tuple[List[List[float]], List[str]]:
         """Get the embedding for a file."""
-        file_text = EmbeddingManager._extract_text_from_file(filepath)
-        content_hash = EmbeddingManager._calculate_hash(file_text)
-        sequences = EmbeddingManager._split_text(file_text)
-
-        # Check if a file with the same content_hash exists
-        existing_file = FileEmbedding.query.filter_by(content_hash=content_hash).first()
-        if existing_file:
-            return existing_file
+        if file_content.file_embeddings:
+            return file_content.file_embeddings
         
-        # Check if a file with the same filepath exists
-        existing_file_by_path = FileEmbedding.query.filter_by(filepath=filepath).first()
+        if not file_content.text_content:
+            raise ValueError("No text content to create embeddings")
+        
+        same_text = FileContent.query.filter_by(text_content_hash=file_content.text_content_hash).first()
+        if same_text:
+            return same_text.file_embeddings
+        
+        sequences = EmbeddingManager._split_text(file_content.text_content)
         sequence_hashes = [EmbeddingManager._calculate_hash(slice) for slice in sequences]
+
+        # Check if a file with the same filepath exists
         new_file_embedding = FileEmbedding(
-            document_id=document_id,
-            filepath=filepath,
-            non_text_content=non_text_content,
-            content_hash=content_hash,
-            sequence_ids=[],
+            content=file_content,
         )
         db.session.add(new_file_embedding)
         db.session.flush() # This assigns an ID to `new_file` without committing
 
+        total_sequences = []
         missing_sequences = []
-        if not existing_file_by_path:
-            missing_sequences = list(zip(sequences, sequence_hashes))
-        else:
-            # Update or insert sequences
-            for content_slice, slice_hash in zip(sequences, sequence_hashes):
-                existing_sequence = SequenceEmbedding.query.filter_by(sequence_hash=slice_hash).first()
-                if not existing_sequence:
-                    missing_sequences.append((content_slice, slice_hash))
-                    continue
+        # Update or insert sequences
+        for content_slice, slice_hash in zip(sequences, sequence_hashes):
+            existing_sequence = SequenceEmbedding.query.filter_by(sequence_hash=slice_hash).first()
+            if not existing_sequence:
+                missing_sequences.append((content_slice, slice_hash))
+                continue
 
-                new_file_embedding.sequence_ids.append(existing_sequence.id)
+            total_sequences.append(existing_sequence)
 
         # Use ThreadPoolExecutor to parallelize embedding generation
         with ThreadPoolExecutor() as executor:
             embeddings = list(executor.map(EmbeddingManager._get_single_embedding, missing_sequences))
 
+
         new_sequences = [
             SequenceEmbedding(
-                file_id=new_file_embedding.id,
                 sequence_hash=slice_hash,
                 sequence_text=content_slice,
                 embedding=embedding,
+                file=file_content
             ) for content_slice, (embedding, slice_hash) in zip(missing_sequences, embeddings)
         ]
+        total_sequences.extend(new_sequences)
+
 
         db.session.add_all(new_sequences)
         db.session.flush()
 
-        new_file_embedding.sequence_ids = [sequence.id for sequence in new_sequences]
+        new_file_embedding.sequences = total_sequences
         db.session.commit()
         
         return new_file_embedding
+        
+    @staticmethod
+    def _get_document_embeddings(document : Document) -> Tuple[List[List[float]], List[str]]:
+        """Get the embedding for a document."""
+        pass
+
+    @staticmethod
+    def get_embeddings(file: Union[FileContent, Document]) -> Tuple[List[List[float]], List[str]]:
+        if isinstance(file, FileContent):
+            return EmbeddingManager._get_file_content_embeddings(file)
+        elif isinstance(file, Document):
+            return EmbeddingManager._get_document_embeddings(file)
+        else:
+            raise ValueError(f"get_embeddings expects either a Document or a FileContent object")
+        
 
     @staticmethod
     def find_similar_files(text: str, limit: int = 5):
