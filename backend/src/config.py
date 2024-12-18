7e605dc8cedddb22748e9d1fe5c67c5a211bca7e
Laurenz Ruzicka <Laurenz.Ruzicka@ait.ac.at>
2024-12-18 17:14:04 +0100

    hide emit messages

--- backend/src/config.py
+++ backend/src/config.py
@@ -1,6 +1,7 @@
 class Config:
     SECRET_KEY = 'secret!'
     DEBUG = True
+    SHOW_EMIT_SUCCESS = False
     # Allow connections from React development server
     CORS_ORIGINS = ["http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:3000/", "*"] # http://localhost:3000
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1234@localhost:5432/eddy_db'
